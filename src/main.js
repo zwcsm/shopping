d066e24f42cdd1c51a5074eb9c34584c87bbebfc
zwcsm <2272837642@qq.com>
2024-12-07 16:11:42 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import '@/styles/common.scss'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import { getCategoryAPI } from './apis/testAPI'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+getCategoryAPI().then(res=>{
+  console.log(res);
+})
+
+app.mount('#app')
