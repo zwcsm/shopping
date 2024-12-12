43b6a5048e0ab4d9db53e45beab2f90be11d2f6c
vily3456 <1095536664@qq.com>
2024-12-12 09:13:43 +0800

    login

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,20 +1,32 @@
 import axios from 'axios'
-
-// 创建axios实例
+import {ElMessage} from 'element-plus';
+import 'element-plus/theme-chalk/el-message.css'
 const http = axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
   timeout: 5000
-})
-
+});
 // axios请求拦截器
 // 一般会进行token身份验证等
 http.interceptors.request.use(config => {
+  /**const userStore = useUserStore();
+  const token = userStore.userInfo.token;
+  if(token){
+      config.headers.Authorization = `Bearer ${token}`
+  }*/
   return config
 }, e => Promise.reject(e))
 
 // axios响应式拦截器
 // 一般进行错误的统一提示，token失效的处理等
-http.interceptors.response.use(res => res.data, e => {
+http.interceptors.response.use(res => res.data, e => {    
+  //统一错误提示
+  ElMessage({type: 'error', message: e.response.data.message})
+  //401token失效处理
+  const userStore = useUserStore();
+  if(e.response.status === 401){
+      userStore.clearUserInfo()
+      router.push('/login')
+  }
   return Promise.reject(e)
 })
 
